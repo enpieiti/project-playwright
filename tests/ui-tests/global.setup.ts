bdd3d05224ff9ce04ed49362baf28d0a6b13e57c
enpieiti <enpieiti@gmail.com>
2026-01-05 21:03:11 +0700

    set timeout

--- tests/ui-tests/global.setup.ts
+++ tests/ui-tests/global.setup.ts
@@ -6,7 +6,9 @@ test("Global setup for auto login", async ({ page, commonUtils, loginPage, dashb
   const decryptedPassword = commonUtils.decryptData(process.env.PASSWORD!);
   await loginPage.gotoOrangeHrm();
   await loginPage.loginOrangeHrm(decryptedUserName, decryptedPassword);
-  await page.waitForURL(`${process.env.BASE_URL}/web/index.php/dashboard/index`);
+  await page.waitForURL(`${process.env.BASE_URL}/web/index.php/dashboard/index`, {
+    timeout: 60000,
+  });
   await expect(dashboardPage.dashboardTitleText).toHaveText("Dashboard");
   await page.context().storageState({
     path: "./playwright/.auth/auth.json",
